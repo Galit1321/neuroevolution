5e9014f112ae7df1470f4e51958ce1c77795a637
galit <galit1321@gmail.com>
2018-06-10 19:45:43 +0300

    Add mnist

--- GA.py
+++ GA.py
@@ -1,22 +1,38 @@
 import numpy as np
-
+from mnist import MNIST
 from NN import NeuralNetwork
 
 
-def setup(mutation_function):
-    for i in range(0,20):
-        population.append(NeuralNetwork(28*28,100))
+def setup(init_pop):
+    mndata = MNIST('./mnist_data')
+    mndata.gz = True
+    images, labels = mndata.load_training()
+    test_images, test_labels = mndata.load_testing()
+    all_data = list(zip(images, labels))
+    valid_data = list(zip(test_images, test_labels))
+    '''create a population
+    loop:
+        eval fitness of nn and creating a new  population by
+        -pick a parent based on fitness score ,mapped to prop
+        -crossover
+        -mutation '''
+    fitness = {}
+    for i in range(0, init_pop):
+        population.append(NeuralNetwork(28 * 28, 100))
 
-    brain = NeuralNetwork(100, 100)
-    child = NeuralNetwork(brain, 100)
-    mutate(child, mutation_function)
+    for net in population:
+        # np.random.choice(all_data, 100, replace=False)
+        net.train(all_data, valid_data, 10)
+        fitness[net.accuracy] = net
+    print(fitness.values())
 
 
 def mutate(model, func):
     new_weight = {}
     for key, value in model.weights:
         new_weight[key] = np.apply_along_axis(func, 1, np.array(value))
-    model.weights = new_weight
+    model.set_weights(new_weight)
 
 
 population = []
+setup(2)
--- NN.py
+++ NN.py
@@ -9,9 +9,9 @@ class ActivationFunction:
 
 sigmoid = lambda x: 1 / (1 + np.exp(-x))
 
-
 sigmoidAF = ActivationFunction(sigmoid,
-        lambda z: np.multiply(np.array(sigmoid(np.array(z))), np.array((1 - sigmoid(np.array(z))))))
+                               lambda z: np.multiply(np.array(sigmoid(np.array(z))),
+                                                     np.array((1 - sigmoid(np.array(z))))))
 
 tanh = ActivationFunction(np.tanh, lambda x: 4 / (np.power(np.exp(-x) + np.exp(x), 2)))
 
@@ -22,96 +22,88 @@ def softmax(x):
 
 
 class NeuralNetwork():
-    def __init__(self, a, b):
+    def __init__(self, a, b, acfunc=sigmoidAF, lr=0.01):
         if isinstance(a, NeuralNetwork):
             self.input_layer = a.input_layer
             self.hidden_layer = a.hidden_layer
             self.output_layer = 10
             self.weights = a.weights
+            self.activation_function = a.activation_function
+            self.accuracy = 0.0
         else:
             self.input_layer = a
             self.hidden_layer = b
-            self.output_nodes = 10
-            glorot_init = np.sqrt(6 / (1.0 * (self.hidden_layer + self.input_layer))).randomize()
+            self.output_layer = 10
+            glorot_init = np.sqrt(6 / (1.0 * (self.hidden_layer + self.input_layer)))
             W1 = np.matrix(
-                np.random.uniform(-1 * glorot_init, glorot_init, (self.hidden_layer, self.input_layer))).randomize()
+                np.random.uniform(-1 * glorot_init, glorot_init, (self.hidden_layer, self.input_layer)))
             W2 = np.matrix(np.random.uniform(-1 * glorot_init, glorot_init, (self.output_layer, self.hidden_layer)))
             b1 = np.transpose(
-                np.matrix(np.random.uniform(-1 * glorot_init, glorot_init, self.hidden_layer)).randomize())
+                np.matrix(np.random.uniform(-1 * glorot_init, glorot_init, self.hidden_layer)))
             b2 = np.transpose(
-                np.matrix(np.random.uniform(-1 * glorot_init, glorot_init, self.output_layer)).randomize())
+                np.matrix(np.random.uniform(-1 * glorot_init, glorot_init, self.output_layer)))
             self.weights = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
-        self.lr = self.setLearningRate()
-        self.accuracy=0.0
-        self.activation_function = ActivationFunction(sigmoid, sigmoidPrime)
-
-    def setLearningRate(self, learning_rate=0.1):
-        self.lr = learning_rate
-
-    def setActivationFunction(func=sigmoid):
-        activation_function = func
+        self.lr = lr
+        self.accuracy = 0.0
+        self.activation_function = acfunc
 
     def updateWeights(self, gradients):
         self.weights = {'W1': (self.weights['W1'] - self.lr * gradients['W1']),
                         'b1': (self.weights['b1'] - self.lr * gradients['b1'])
-            ,'W2': (self.weights['W2'] - self.lr * gradients['W2'])
-            ,'b2': (self.weights['b2'] - self.lr * gradients['b2'])}
+            , 'W2': (self.weights['W2'] - self.lr * gradients['W2'])
+            , 'b2': (self.weights['b2'] - self.lr * gradients['b2'])}
 
+    def set_weights(self, weights):
+        self.weights = weights
 
+    def forward(self, x, y):
+        # Follows procedure given in notes
+        W1, b1, W2, b2 = [self.weights[key] for key in ('W1', 'b1', 'W2', 'b2')]
+        x = np.transpose(np.matrix(x))
+        z1 = np.add(np.dot(W1, x), b1)
+        h1 = self.activation_function.func(z1)  # activation function
+        z2 = np.add(np.dot(W2, h1), b2)
+        h2 = softmax(z2)
+        loss = -(np.log(h2[int(y)]))
+        y_vec = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        y_vec[int(y)] = 1
+        y_vec = np.transpose(np.matrix(y_vec))
+        ret = {'x': x, 'y': y_vec, 'z1': z1, 'h1': h1, 'z2': z2, 'h2': h2, 'loss': loss}
 
+        return ret
 
+    def checkValidation(self, validation_set):
+        right_exmp = 0.0
+        for x, y in validation_set:
+            val_func = self.forward(x, y)
+            if ((np.argmax(val_func['h2'])) == int(y)):
+                right_exmp = right_exmp + 1.0
+        return right_exmp
 
-def forward(x, y, model):
-    # Follows procedure given in notes
-    W1, b1, W2, b2 = [model.weight[key] for key in ('W1', 'b1', 'W2', 'b2')]
-    x = np.transpose(np.matrix(x))
-    z1 = np.add(np.dot(W1, x), b1)
-    h1 = model.activation_fun(z1)  # activation function
-    z2 = np.add(np.dot(W2, h1), b2)
-    h2 = softmax(z2)
-    loss = -(np.log(h2[int(y)]))
-    y_vec = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    y_vec[int(y)] = 1
-    y_vec = np.transpose(np.matrix(y_vec))
-    ret = {'x': x, 'y': y_vec, 'z1': z1, 'h1': h1, 'z2': z2, 'h2': h2, 'loss': loss}
+    def bprop(self, fprop_cache):
+        w2 = self.weights['W2']
+        x = fprop_cache['x']
+        y = fprop_cache['y']
+        z1 = fprop_cache['z1']
+        h1 = fprop_cache['h1']
+        h2 = fprop_cache['h2']
+        dz2 = np.subtract(h2, y)
+        dW2 = np.dot(dz2, np.transpose(h1))  # dL/dz2 * dz2/dw2
+        db2 = dz2  # dL/dz2 * dz2/db2
+        dz1 = np.multiply(np.array(np.dot(np.transpose(w2), dz2)),
+                          np.array(self.activation_function.dfunc(z1)))  # dL/dz2 * dz2/dh1 * dh1/dz1
+        dW1 = np.dot(dz1, np.transpose(x))  # dL/dz2 * dz2/dh1 * dh1/dz1 * dz1/dw1
+        db1 = dz1  # dL/dz2 * dz2/dh1 * dh1/dz1 * dz1/db1
+        return {'b1': db1, 'W1': dW1, 'b2': db2, 'W2': dW2}
 
-    return ret
-
-
-def checkValidation(validation_set, model):
-    right_exmp = 0.0
-    for x, y in validation_set:
-        val_func = forward(x, y, model)
-        if ((np.argmax(val_func['h2'])) == int(y)):
-            right_exmp = right_exmp + 1.0
-    return right_exmp
-
-
-def bprop(fprop_cache, model):
-    w2 = fprop_cache['W2']
-    x = fprop_cache['x']
-    y = fprop_cache['y']
-    z1 = fprop_cache['z1']
-    h1 = fprop_cache['h1']
-    h2 = fprop_cache['h2']
-    dz2 = np.subtract(h2, y)
-    dW2 = np.dot(dz2, np.transpose(h1))  # dL/dz2 * dz2/dw2
-    db2 = dz2  # dL/dz2 * dz2/db2
-    dz1 = np.multiply(np.array(np.dot(np.transpose(w2), dz2)),
-                      np.array(model.activation_fun.dfunc(z1)))  # dL/dz2 * dz2/dh1 * dh1/dz1
-    dW1 = np.dot(dz1, np.transpose(x))  # dL/dz2 * dz2/dh1 * dh1/dz1 * dz1/dw1
-    db1 = dz1  # dL/dz2 * dz2/dh1 * dh1/dz1 * dz1/db1
-    return {'b1': db1, 'W1': dW1, 'b2': db2, 'W2': dW2}
-
-
-def train(model, training_set, validation_set, epochs):
-    for i in range(0, epochs):
-        np.random.shuffle(training_set)
-        for x, y in training_set:
-            functions = forward(x, y, model)  # forward
-            bp_gradients = bprop(functions, model)  # backpropagation
-            # update weights
-            model.updateWeights(bp_gradients)
-        right = checkValidation(validation_set, model)
-        model.acc=right / float(len(validation_set))*100.0
-        print(i, "success percentage:" + str(model.acc) + '%')
+    def train(self, training_set, validation_set, epochs):
+        for i in range(0, epochs):
+            np.random.shuffle(training_set)
+            for x, y in training_set:
+                functions = self.forward(x, y)  # forward
+                bp_gradients = self.bprop(functions)  # backpropagation
+                # update weights
+                self.updateWeights(bp_gradients)
+            right = self.checkValidation(validation_set)
+            self.acc = right / float(len(validation_set)) * 100.0
+            print(i, "success percentage:" + str(self.acc) + '%')
