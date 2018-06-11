8fde35014b23f9919114a704fca6ad002ba9ad73
galit <galit1321@gmail.com>
2018-06-11 17:49:53 +0300

    crossover

--- GA.py
+++ GA.py
@@ -18,34 +18,51 @@ def setup(init_pop):
         -mutation '''
     fitness = {}
     for i in range(0, init_pop):
-        population.append(NeuralNetwork(500, 0.01))
-    #indices = list(range(len(all_data)))
-    test_indices = list(range(len(valid_data)))
-    for net in population:
-        # validation_idx=np.random.choice(indices, size=784,replace=False)
-        test_idx = np.random.choice(test_indices, size=784, replace=False)
-        # net.train(createSub(all_data,validation_idx), createSub(valid_data,test_idx), 10)
-        loss = net.checkValidation(create_sub(valid_data, test_idx))
-        fitness[loss] = net
-    sorted(fitness)
+        population.append(NeuralNetwork(100, 0.01))
+    # indices = list(range(len(all_data)))
+    for i in range(0, 50):
+        test_indices = list(range(len(valid_data)))
+        for net in population:
+            # validation_idx=np.random.choice(indices, size=784,replace=False)
+            test_idx = np.random.choice(test_indices, size=784, replace=False)
+            # net.train(createSub(all_data,validation_idx), createSub(valid_data,test_idx), 10)
+            loss = net.checkValidation(create_sub(valid_data, test_idx))
+            fitness[loss] = net
+        sort_dict = sorted(fitness)
 
-    print(crossover(list(fitness.values())[0].weights['W1'], list(fitness.values())[1].weights['W1']))
+        children = []
+        while len(children) > init_pop - 5:
+            weight1 = fitness[sort_dict[0]].weights
+            sort_dict.pop(0)
+            weight2 = fitness[sort_dict[0]].weights
+            sort_dict.pop(0)
+            children.append(crossover(weight1, weight2))
     print(fitness.keys())
 
 
 def crossover(weight1, weight2):
-    dict_res = {}
+    child1 = NeuralNetwork(100)
+    child2 = NeuralNetwork(100)
+    dict_res1 = {}
+    dict_res2 = {}
     for key, val in weight1:
         father = weight2[key]
-        res = np.zeros((val.shape[0], val.shape[1]))
+        res1 = np.zeros((val.shape[0], val.shape[1]))
+        res2 = np.zeros((val.shape[0], val.shape[1]))
         prob = np.random.uniform(0, 1)
         for i in range(0, val.shape[0]):
             if prob > np.random.rand():
-                res[i] = val[i]
+                res1[i] = val[i]
+                res2[i] = father[i]
             else:
-                res[i] = father[i]
-        dict_res[key] = res
-    return dict_res
+                res2[i] = val[i]
+                res1[i] = father[i]
+
+        dict_res1[key] = res1
+        dict_res2[key] = res2
+    child1.set_weights(dict_res1)
+    child2.set_weights(dict_res2)
+    return (child1, child2)
 
 
 def create_sub(data, indicte):
@@ -53,6 +70,21 @@ def create_sub(data, indicte):
     for num in indicte:
         res.append(data[num])
     return res
+
+
+def roulette_wheel_selection(fitness_dict, size_select):
+    sum = np.sum(list(fitness_dict.key()))
+    p = np.random.uniform(0, sum)
+    chosen = []
+    for inter in range(size_select):
+        part_sum = 0.0
+        for key, val in fitness_dict:
+            if part_sum > p:
+                chosen.append(val)
+                del fitness_dict[key]
+                break
+            part_sum += key
+    return chosen
 
 
 def mutate(model, func):
@@ -63,4 +95,4 @@ def mutate(model, func):
 
 
 population = []
-setup(2)
+setup(20)
